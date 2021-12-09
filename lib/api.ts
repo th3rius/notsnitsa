99f036ca7eda9da5600838e7127a12cd753b2160
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2021-12-09 18:50:42 -0300

    add non-null assertion to environment variables

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import GhostContentAPI from "@tryghost/content-api";
 
-const GHOST_API_URL = process.env.GHOST_API_URL;
-const GHOST_API_KEY = process.env.GHOST_API_KEY;
+const GHOST_API_URL = process.env.GHOST_API_URL!;
+const GHOST_API_KEY = process.env.GHOST_API_KEY!;
 
 const api = new GhostContentAPI({
   url: GHOST_API_URL,
