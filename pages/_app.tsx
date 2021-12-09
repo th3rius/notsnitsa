9ae02054c886e9817ca6592493ad11e114b8bec7
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2021-12-09 15:01:38 -0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,41 @@
 import "../styles/globals.css";
+import {useEffect} from "react";
+import Script from "next/script";
+import {useRouter} from "next/router";
+import * as gtag from "../lib/gtag";
+import {AppProps} from "next/app";
 
-function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />;
+function App({Component, pageProps}: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    function handleRouteChange(url: string) {
+      gtag.pageview(url);
+    }
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script id="gtag-init">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname
+        });
+      `}</Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
