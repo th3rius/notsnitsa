748231239bea04eefeeb3afddc81a48310f1fbef
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2021-12-15 17:59:01 -0300

    add html lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,33 @@
-import Document, {Head, Html, Main, NextScript} from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import api from "../lib/api";
+import {SettingsResponse} from "@tryghost/content-api";
 
-class MyDocument extends Document {
+export interface MyDocumentProps {
+  settings: SettingsResponse;
+}
+
+class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const settings = await api.settings.browse();
+    return {...initialProps, settings};
+  }
+
   render() {
     return (
-      <Html>
+      <Html lang={this.props.settings.lang}>
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Lora&family=Open+Sans&display=swap"
             rel="stylesheet"
           />
+          <meta property="og:title" />
         </Head>
         <body>
           <Main />
