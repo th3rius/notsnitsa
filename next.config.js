59c5e132401d7431e12e2ddbef9d8be2b9d31cc0
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2021-12-15 10:59:59 -0300

    add sitemap.xml

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@
  */
 module.exports = {
   reactStrictMode: true,
+  rewrites: function () {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/_sitemap",
+      },
+    ];
+  },
 };
---
+++ pages/_sitemap.tsx
@@ -0,0 +1,47 @@
+import {GetServerSideProps} from "next";
+import api from "../lib/api";
+import {DateTime} from "luxon";
+import xml from "xml";
+
+// Generates a dynamic sitemap.xml. A sitemap defines the
+// relationship between pages of the site. Search engines
+// utilize this file to more accurately index the site.
+// See: https://developers.google.com/search/docs/advanced/sitemaps/overview
+export const getServerSideProps: GetServerSideProps = async ({res}) => {
+  const posts = await api.posts.browse({limit: "all"});
+  res.setHeader("Content-Type", "text/xml");
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=1200, stale-while-revalidate=600"
+  );
+  res.write(
+    xml(
+      {
+        urlset: [
+          {_attr: {xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9"}},
+          ...posts.map((post) => ({
+            url: [
+              {
+                loc: post.url,
+              },
+              {
+                lastmod: DateTime.fromISO(post.updated_at!).toISODate(),
+              },
+            ],
+          })),
+        ],
+      },
+      {declaration: true}
+    )
+  );
+  res.end();
+  return {
+    props: {},
+  };
+};
+
+function Sitemap() {
+  return null;
+}
+
+export default Sitemap;
