6bab5c8ec82b9d1308806b5c607988f52f983acc
Gabriel de Oliveira <gabriel.o03@aluno.ifsc.edu.br>
2021-12-17 16:11:29 -0300

    fix sitemap url generation

--- pages/_sitemap.tsx
+++ pages/_sitemap.tsx
@@ -3,6 +3,8 @@ import api from "../lib/api";
 import {DateTime} from "luxon";
 import xml from "xml";
 
+const BLOG_URL = process.env.BLOG_URL!;
+
 // Generates a dynamic sitemap.xml. A sitemap defines the
 // relationship between pages of the site. Search engines
 // utilize this file to more accurately index the site.
@@ -22,7 +24,7 @@ export const getServerSideProps: GetServerSideProps = async ({res}) => {
           ...posts.map((post) => ({
             url: [
               {
-                loc: post.url,
+                loc: `${BLOG_URL}/${post.slug}`,
               },
               {
                 lastmod: DateTime.fromISO(post.updated_at!).toISODate(),
